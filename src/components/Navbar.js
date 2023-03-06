7766766cecdcd26ed9b3757a7b9d46c4f5ca1b5c
Mirella <mirella.dimichino@gmail.com>
2023-03-06 19:39:55 +0000

    added links to pages/active now/ small mods on css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,16 +13,16 @@ const Navbar = () => {
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item active">
-          <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          <a className="nav-link" href="/">Home <span class="sr-only"></span></a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">About</a>
+          <a className="nav-link" href="/about">About</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Projects</a>
+          <a className="nav-link" href="/projects">Projects</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled" href="#">Contact</a>
+          <a className="nav-link" href="/contact">Contact</a>
         </li>
       </ul>
     </div>
