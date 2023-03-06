dd50eea50cf51ac184801cd677b01cbc6e08d0bf
Mirella <mirella.dimichino@gmail.com>
2023-03-06 13:45:24 +0000

    adding some css/navbar links/starting my protfolio

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Projects from "./routes/Projects";
 import About from "./routes/About";
 import Contact from "./routes/Contact";
 //import { Route, RouterProvider } from "react-router-dom";
-import { Routes,Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
+  return (//created routes to different pages
     <>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/project" element={<Projects />} />
+      <Route path="/projects" element={<Projects />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
 
