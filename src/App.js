4b9562dee15e9da2bf1309ab4144dac88aace661
Mirella <mirella.dimichino@gmail.com>
2023-03-06 12:44:31 +0000

    created routes to the different pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import "./index.css";// imported css
+import Home from "./routes/Home";
+import Projects from "./routes/Projects";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+//import { Route, RouterProvider } from "react-router-dom";
+import { Routes,Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1>This is my Portfolio</h1>
-      
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/project" element={<Projects />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+
+
+      </Routes>
+      </>
   );
 }
 
