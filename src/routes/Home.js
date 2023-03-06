c9e74d873a18e8c9db9be6f78d27d038674361bd
Mirella <mirella.dimichino@gmail.com>
2023-03-06 22:57:24 +0000

    adding first project img

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,7 +4,31 @@ const Home=()=> {
     return (
         <div>
             <Navbar></Navbar>
-        </div>
+            <div className="jumbotron">
+  <h1 className="display-4 text-dark">Hi , I am Mirella</h1>
+  <p className="lead">I am a Junior web developer </p>
+  <hr className="my-4"/>
+  <p>It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
+  <p className="lead">
+    <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
+<div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img className="d-block w-100" src={require('./')} alt="First slide" />
+    </div>
+    <div className="carousel-item">
+      <img className="d-block w-100" src="..." alt="Second slide" />
+    </div>
+    <div className="carousel-item">
+      <img className="d-block w-100" src="..." alt="Third slide" />
+    </div>
+  </div>
+</div>
+
+</div>
+
     )
 }
  export default Home
