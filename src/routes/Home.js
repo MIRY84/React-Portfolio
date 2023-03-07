de049b0cbfd31307aa2db73b5e07ddf1f607d654
Mirella <mirella.dimichino@gmail.com>
2023-03-07 16:31:27 +0000

    adding card projects

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+
 const Home=()=> {
     return (
         <div>
@@ -12,23 +13,11 @@ const Home=()=> {
   <p className="lead">
     <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
   </p>
-</div>
-<div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img className="d-block w-100" src={require('./')} alt="First slide" />
-    </div>
-    <div className="carousel-item">
-      <img className="d-block w-100" src="..." alt="Second slide" />
-    </div>
-    <div className="carousel-item">
-      <img className="d-block w-100" src="..." alt="Third slide" />
-    </div>
   </div>
-</div>
 
-</div>
-
-    )
+    
+    </div>
+    
+ );
 }
- export default Home
+ export default Home;
--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,8 +1,51 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import myImage from './images/Quick-movies3.png';
+import myImage2 from './images/My-team.png';
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div>
+      <CardGroup>
+      <Card>
+        <Card.Img src={myImage}></Card.Img>
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+        
+      </Card>
+      <Card>
+        <Card.Img src={myImage2}></Card.Img>
+       
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This card has supporting text below as a natural lead-in to
+            additional content.{' '}
+          </Card.Text>
+        </Card.Body>
+        
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </Card.Text>
+        </Card.Body>
+       
+      </Card>
+    </CardGroup>
+
+    </div>
   )
 }
 
