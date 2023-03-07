c2a805d310b881918fce80d2930400de3b9b973c
Mirella <mirella.dimichino@gmail.com>
2023-03-07 16:56:26 +0000

    added more content to about section/ project/home section

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -8,42 +8,55 @@ const Projects = () => {
   return (
     <div>
       <CardGroup>
-      <Card>
-        <Card.Img src={myImage}></Card.Img>
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </Card.Text>
-        </Card.Body>
-        
-      </Card>
-      <Card>
-        <Card.Img src={myImage2}></Card.Img>
+        <Card>
+          <Card.Img src={myImage}></Card.Img>
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural lead-in
+              to additional content. This content is a little bit longer.
+            </Card.Text>
+          </Card.Body>
+         </Card>
+
+        <Card>
+          <Card.Img src={myImage2}></Card.Img>
+
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to
+              additional content.{' '}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card>
+          <Card.Img src={myImage2}></Card.Img>
+
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to
+              additional content.{' '}
+            </Card.Text>
+          </Card.Body>
+          <Card>
+            <Card.Img src={myImage2}></Card.Img>
+
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This card has supporting text below as a natural lead-in to
+                additional content.{' '}
+              </Card.Text>
+            </Card.Body>
+
+          </Card>
+          </Card>
+
        
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This card has supporting text below as a natural lead-in to
-            additional content.{' '}
-          </Card.Text>
-        </Card.Body>
-        
-      </Card>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This card has even longer content than the
-            first to show that equal height action.
-          </Card.Text>
-        </Card.Body>
-       
-      </Card>
-    </CardGroup>
+      </CardGroup>
 
     </div>
   )
