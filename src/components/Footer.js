783352af40f638ecce6d40e9e5eb4f21d41b20e2
Mirella <mirella.dimichino@gmail.com>
2023-03-08 13:40:55 +0000

    changing home/footer and added icons/ first attempt

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import InstagramIcon from "@material-ui/icons/Instagram";
+import { FaFacebook } from 'react-icons/fa';
+import {FaTwitter} from 'react-icons/fa';
+import {AiTwotoneMail} from 'react-icons/ai';
+import "../routes/styles/Footer.css";
+
+
 
 const Footer = () => {
   return (
     <div className= "footer">
-     <div></div>
+     <div>
+     <AiTwotoneMail />
+       
+        <FaTwitter />
+        <FaFacebook />
+     </div>
      <p> &copy; 2023 mirella.com</p>
      </div>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,51 +1,45 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { FaFacebook } from 'react-icons/fa';
+import {FaTwitter} from 'react-icons/fa';
+import {AiTwotoneMail} from 'react-icons/ai';
+import "../routes/styles/Footer.css";
 
 const Home=()=> {
     return (
         <div>
-            <Navbar></Navbar>
-            <div className="jumbotron">
-  <h1 className="display-4 text-dark">Hi , I am Mirella</h1>
-  <p className="lead">I am a Junior web developer </p>
-  <hr className="my-4"/>
-  <p>It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
-  <p className="lead">
-  Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-        architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-        aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-        consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
-        dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-        exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
-        consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
-        molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-      <p>
-        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-        voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati
-        cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id
-        est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam
-        libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod
-        maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.
-        Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut
-        et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a
-        sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis
-        doloribus asperiores repellat.
-      </p>
+            <Navbar />
+            <div className="home">
+              <div className="about">
+                <h2> Hi I am Mirellla a web developer with a passion for best performsnces </h2>
+              <div className="prompt">
+                <p> Web developer with a degree in Administration
+            voluptatibus maiores alias consequatur aut perferendis
+        doloribus asperiores repellat.</p>
+        
+        <AiTwotoneMail />
+       
+        <FaTwitter />
+        <FaFacebook />
+    
 
   </div>
+</div>
+</div>
 
-    
-    </div>
+ <div className="skills">
+  <h1>Skills</h1>
+  <ol className="list">
+    <li className="item">
+      <h2>FrontEnd</h2>
+    <span>
+      React, Html, css, Bootstrap, Jquery, Javascript
+    </span>
+    </li>
+  </ol>
+ </div>
+ </div>
+
     
  );
 }
