f78b0aa0c9836a3c1f2928b75ff16dc2ab1e4a8f
Mirella <mirella.dimichino@gmail.com>
2023-03-09 11:50:29 +0000

    adding project.json/card/projects

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,66 +1,41 @@
 import React from 'react'
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
-import myImage from './images/Quick-movies3.png';
-import myImage2 from './images/My-team.png';
+import projects from "../components/Projects/Project.json";
+import ProjectCard from "../components/ProjectCard";
 
-const Projects = () => {
+function ProjectGallery() {
   return (
-    <div>
-      <CardGroup>
-        <Card>
-          <Card.Img src={myImage}></Card.Img>
-          <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-              This is a wider card with supporting text below as a natural lead-in
-              to additional content. This content is a little bit longer.
-            </Card.Text>
-          </Card.Body>
-         </Card>
-
-        <Card>
-          <Card.Img src={myImage2}></Card.Img>
-
-          <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-              This card has supporting text below as a natural lead-in to
-              additional content.{' '}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-
-        <Card>
-          <Card.Img src={myImage2}></Card.Img>
-
-          <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-              This card has supporting text below as a natural lead-in to
-              additional content.{' '}
-            </Card.Text>
-          </Card.Body>
-          <Card>
-            <Card.Img src={myImage2}></Card.Img>
-
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This card has supporting text below as a natural lead-in to
-                additional content.{' '}
-              </Card.Text>
-            </Card.Body>
-
-          </Card>
-          </Card>
-
-       
-      </CardGroup>
-
+    <div className="row">
+      <div className="col-12">
+      <h1>ProjectGallery</h1>
+      <p>
+        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula. Curabitur pellentesque
+        turpis sit amet eros iaculis, a mollis arcu dictum. Ut vel ante eget massa ornare placerat.
+        Etiam nisl orci, finibus sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum
+        nunc, sed pretium risus rutrum eget. Nam consequat, ligula in faucibus vestibulum, nisi
+        justo laoreet risus, luctus luctus mi lacus sit amet libero. Class aptent taciti sociosqu ad
+        litora torquent per conubia nostra, per inceptos himenaeos. Mauris pretium condimentum
+        tellus eget lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec
+        placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum in velit pellentesque
+        vehicula sit amet at augue. Maecenas aliquam bibendum congue. Pellentesque semper, lectus
+        non ullamcorper iaculis, est ligula suscipit velit, sed bibendum turpis dui in sapien.
+      </p>
+      </div>
+      
+        {projects.map(project => (
+          <ProjectCard
+            key={project.id}
+            name={project.name}
+            deployedLink={project.deployedLink}
+            gitHubLink={project.gitHubLink}
+            image={project.image}
+          />
+      ))}      
+      
     </div>
-  )
+  );
 }
 
-export default Projects
+export default ProjectGallery;
+
+
 
