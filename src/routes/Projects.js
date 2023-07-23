ac457cb990faaaa57c3fd88c117b6fe8f42b9d9f
Mirella <mirella.dimichino@gmail.com>
2023-07-23 11:11:41 +0100

    fixing the project page/adding another project

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -9,16 +9,10 @@ function ProjectGallery() {
       <div className="col-12">
       <h1>ProjectGallery</h1>
       <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula. Curabitur pellentesque
-        turpis sit amet eros iaculis, a mollis arcu dictum. Ut vel ante eget massa ornare placerat.
-        Etiam nisl orci, finibus sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum
-        nunc, sed pretium risus rutrum eget. Nam consequat, ligula in faucibus vestibulum, nisi
-        justo laoreet risus, luctus luctus mi lacus sit amet libero. Class aptent taciti sociosqu ad
-        litora torquent per conubia nostra, per inceptos himenaeos. Mauris pretium condimentum
-        tellus eget lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec
-        placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum in velit pellentesque
-        vehicula sit amet at augue. Maecenas aliquam bibendum congue. Pellentesque semper, lectus
-        non ullamcorper iaculis, est ligula suscipit velit, sed bibendum turpis dui in sapien.
+       These are all the challenges and projects done during the bootcamp and beyond it. The latest work is the portfolio of my brother.
+       I used all the recent technologies in particular Javascript, React, Vite and the classic HTML and CSS.
+       Please have a look , I would appreciate your honest feedback and ways to improve. 
+      
       </p>
       </div>
       
